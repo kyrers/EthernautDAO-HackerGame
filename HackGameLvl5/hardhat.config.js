42dbfe34a8c0f80fe62bf702b355b412042d44a9
kyrers <kyrers.eth@gmail.com>
2022-07-31 00:44:18 +0100

    Set up for level 5

---
+++ HackGameLvl5/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+
+module.exports = {
+  solidity: "0.8.15",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545"
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: [`${process.env.GOERLI_ACCOUNT_PK}`,]
+    },
+  }
+}
