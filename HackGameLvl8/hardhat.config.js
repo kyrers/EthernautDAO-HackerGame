f0eb7486cb1624dbf0ce26746432ff236972e604
kyrers <kyrers.eth@gmail.com>
2022-12-07 23:30:35 +0000

    Updated hardhat config and gitignore

--- HackGameLvl8/hardhat.config.js
+++ HackGameLvl8/hardhat.config.js
@@ -23,5 +23,9 @@ module.exports = {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`,
       accounts: [`${process.env.GOERLI_ACCOUNT_PK}`, `${process.env.GOERLI_SECOND_ACCOUNT_PK}`]
     },
+    "optimism-goerli": {
+      url: `https://opt-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+      accounts: [`${process.env.OPTIMISTIC_GOERLI_ACCOUNT_PK}`,]
+    }
   }
 }
